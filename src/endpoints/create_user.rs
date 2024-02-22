b7a97b4089aa1c6b4adabfc726ec59fb7fba7c93
jmaralo <martinezalonsojn@gmail.com>
2024-02-22 16:46:06 +0100

    Update error messages

--- src/endpoints/create_user.rs
+++ src/endpoints/create_user.rs
@@ -19,10 +19,7 @@ pub async fn post(State(state): State<Arc<AppState>>, Json(new_user): Json<NewUs
     let user_name = match new_user.validate_name() {
         Ok(name) => name,
         Err(reason) => {
-            debug!(
-                "Sign-in request with invalid username: \"{}\", reason: {}",
-                new_user.name, reason
-            );
+            debug!("Invalid username \"{}\", reason: {}", new_user.name, reason);
             return StatusCode::BAD_REQUEST;
         }
     };
@@ -36,11 +33,11 @@ pub async fn post(State(state): State<Arc<AppState>>, Json(new_user): Json<NewUs
     };
 
     if (*user_list).contains_key(&user_name) {
-        info!("Tried to create already existing user: \"{}\"", user_name);
+        info!("User \"{}\" already exists", user_name);
         return StatusCode::CONFLICT;
     }
 
-    info!("Adding new user: \"{}\"", user_name);
+    info!("Adding new user \"{}\"", user_name);
     user_list.insert(user_name, new_user.password);
     return StatusCode::CREATED;
 }
