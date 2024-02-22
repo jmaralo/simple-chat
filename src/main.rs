47ceccf30daf833768e264efa1a35699bfa41c21
jmaralo <martinezalonsojn@gmail.com>
2024-02-22 01:08:14 +0100

    Add sign-in endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-use axum::{routing::get, Router};
-use tracing::info;
+use std::fmt;
+
+use axum::{
+    extract::Json,
+    http::StatusCode,
+    routing::{get, post},
+    Router,
+};
+use serde::Deserialize;
+use tracing::{debug, info};
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let app = Router::new().route("/", get(root));
+    let app = Router::new().route("/sign-in", post(create_user));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
@@ -13,6 +21,18 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn root() -> &'static str {
-    return "Hello, world!";
+async fn create_user(Json(new_user): Json<NewUser>) -> StatusCode {
+    debug!("Adding new user {}", new_user);
+    return StatusCode::NOT_IMPLEMENTED;
+}
+
+#[derive(Deserialize)]
+struct NewUser {
+    username: String,
+}
+
+impl fmt::Display for NewUser {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.username)
+    }
 }
