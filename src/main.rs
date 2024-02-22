df907cab378ac5c0d9494b4be411173aff28baac
jmaralo <martinezalonsojn@gmail.com>
2024-02-22 01:20:33 +0100

    Add user name validation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,4 @@
-use std::fmt;
-
-use axum::{
-    extract::Json,
-    http::StatusCode,
-    routing::{get, post},
-    Router,
-};
+use axum::{extract::Json, http::StatusCode, routing::post, Router};
 use serde::Deserialize;
 use tracing::{debug, info};
 
@@ -22,17 +15,36 @@ async fn main() {
 }
 
 async fn create_user(Json(new_user): Json<NewUser>) -> StatusCode {
-    debug!("Adding new user {}", new_user);
+    let user_name = match new_user.validate_name() {
+        UserName::Valid(name) => name,
+        UserName::Invalid(name, reason) => {
+            debug!(
+                "Sign-in request with invalid username: \"{}\", reason: {}",
+                name, reason
+            );
+            return StatusCode::BAD_REQUEST;
+        }
+    };
+
+    debug!("Adding new user {}", user_name);
     return StatusCode::NOT_IMPLEMENTED;
 }
 
 #[derive(Deserialize)]
 struct NewUser {
-    username: String,
+    name: String,
 }
 
-impl fmt::Display for NewUser {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.username)
+enum UserName {
+    Valid(String),
+    Invalid(String, &'static str),
+}
+
+impl NewUser {
+    fn validate_name(&self) -> UserName {
+        if self.name.len() > 12 {
+            return UserName::Invalid(self.name.clone(), "name too long");
+        }
+        return UserName::Valid(self.name.clone());
     }
 }
